9854df2a605326f12aedb5bdcb5ad44122eea35b
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-11-30 05:48:42 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('Hello World')
+import boto3
+ec2=boto3.resource ('ec2')
+instance=ec2.create_instances (
+    ImageId='ami-063f64fd624326307',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro'
+)
+print(instance[0].id)
