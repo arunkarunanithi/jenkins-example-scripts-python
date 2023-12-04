70d31dec358b1811a5c11a96f0eb515b40a0f740
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-12-04 17:56:20 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
 import boto3
 from botocore.config import Config
 
+proxy_definitions = {
+    'http': 'http://proxy.amazon.com:6502',
+    'https': 'https://proxy.amazon.org:2010'
+}
+
 my_config = Config(
     region_name='us-east-2',
-    signature_version='v4'
-    )
+    signature_version='v4',
+    proxies=proxy_definitions
+)
 
 client = boto3.client('kinesis', config=my_config)
 
