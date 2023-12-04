cbb26fa2f63031779401c307b393eaef8c40afde
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-12-04 17:20:48 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import boto3
 ec2=boto3.resource ('ec2')
+AWS_DEFAULT_REGION = ec2.describe_regions('us-east-2')
+print(regions)
 instance=ec2.create_instances (
     ImageId='ami-063f64fd624326307',
     MinCount=1,
