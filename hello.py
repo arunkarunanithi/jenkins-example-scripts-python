a3f395f09db1da4ed38322b71f775afa20bffd3f
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-12-04 16:48:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import boto3
 ec2=boto3.resource ('ec2')
+regions = ec2.describe_regions('us-east-2')
+print(regions)
 instance=ec2.create_instances (
-    Region_name='us-east-2'
     ImageId='ami-063f64fd624326307',
     MinCount=1,
     MaxCount=1,
