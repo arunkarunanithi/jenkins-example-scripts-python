bccc9630f1e38d161c072da706804cc1afbb3898
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-12-04 17:13:15 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 import boto3
-ec2=boto3.resource ('ec2')
-region_name='us-east-2'
+ec2=boto3.resource ('ec2', region_name='us-east-2')
 instance=ec2.create_instances (
     ImageId='ami-063f64fd624326307',
     MinCount=1,
