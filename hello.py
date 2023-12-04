3c60fafe989b8d2d0f78d73bcf1faefd9bbda7a6
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-12-04 18:03:20 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,19 +1,5 @@
 import boto3
-from botocore.config import Config
-
-proxy_definitions = {
-    'http': 'http://proxy.amazon.com:6502',
-    'https': 'https://proxy.amazon.org:2010'
-}
-
-my_config = Config(
-    region_name='us-east-2',
-    signature_version='v4',
-    proxies=proxy_definitions
-)
-
-client = boto3.client('kinesis', config=my_config)
-
+kms = boto3.client('kms', region_name='us-east-2')
 ec2=boto3.resource ('ec2')
 instance=ec2.create_instances (
     ImageId='ami-063f64fd624326307',
