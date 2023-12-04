2303419aa222f59002f71c03c486fc3f77afce36
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-12-04 18:18:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 import boto3
+from botocore.config import Config
+my_config = Config(
+    region_name = 'us-east-2'
+)
+
+client = boto3.client('kinesis', config=my_config)
 ec2=boto3.resource ('ec2')
 instance=ec2.create_instances (
     ImageId='ami-063f64fd624326307',
